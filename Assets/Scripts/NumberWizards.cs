5e598e7143f19e4a3798ac2bd91e99329730ef1d
Furkan Kaya <furkan.kaya.web@gmail.com>
2016-09-07 22:54:03 +0300

    Implement guessing logic and complete the project
    Project is done. But it can be further improved by letting the player to
    pick the range, making computer's guesses random, etc.

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -2,31 +2,49 @@
 using UnityEngine;
 
 public class NumberWizards : MonoBehaviour {
+	
+	int max;
+	int min;
+	int guess;
 
-	// Use this for initialization
 	void Start () {
-		print ("Welcome to Number Wizard");
-		print ("Pick a number in your head, but don't tell me!");
-		
-		int max = 10000;
-		int min = 1;
-		
-		print ("The highest number you can pick is " + max);
-		print ("The lowest number you can pick is " + min);
-		
-		print ("Is the number higher or lower than 500?");
-		print ("Up = higher, down = lower, return = equal");
-					
+		StartGame();
 	}
 	
-	// Update is called once per frame
+	void StartGame () {	
+		max = 1000;
+		min = 1;
+		guess = 500;
+	
+		max = max + 1;
+		
+		print ("========================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me!");		
+		
+		print ("The highest number you can pick is " + (max-1));
+		print ("The lowest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than" + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+	}
+	
+	void NextGuess () {
+		guess = (max + min) / 2;
+		print ("Higher or lower than" + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+	}
+	
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("Up arrow pressed");
+			min = guess;
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("Down arrow pressed");
+			max = guess;
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print("I won!");
+			StartGame();
 		}	
 		
 	}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Complete Unity Developer Course, 1st Game: Number Wizard
 
-The game project I created while taking the course and following the video tutorials.
+A game project which is created while taking the course and following the video tutorials.
 Official Course page: http://www.completeunitydeveloper.com/
+
+Number Wizard is a console based mini-game. Game tries to predict the number that the player picked by asking if it is higher or lower than some values.
